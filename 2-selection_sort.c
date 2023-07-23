71125c7564c2ea95dbe85065e2f1e394a3290bea
zakisu250 <zakisu250@gmail.com>
2023-07-23 10:00:32 +0300

    betty error fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,35 +10,25 @@
 
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, k, minIndex;
-    int tmp;
+	size_t i, j, k, minIndex;
+	int tmp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        minIndex = i;
+	for (i = 0; i < size - 1; i++)
+	{
+		minIndex = i;
 
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[minIndex])
-            {
-                minIndex = j;
-            }
-        }
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[minIndex])
+				minIndex = j;
+		}
 
-	if (minIndex != i)
-        {
-            tmp = array[i];
-            array[i] = array[minIndex];
-            array[minIndex] = tmp;
-
-            for (k = 0; k < size; k++)
-            {
-                if (k == size - 1)
-                    printf("%d", array[k]);
-                else
-                    printf("%d, ", array[k]);
-            }
-            printf("\n");
-        }
-    }
+		if (minIndex != i)
+		{
+			tmp = array[i];
+			array[i] = array[minIndex];
+			array[minIndex] = tmp;
+			print_array(array, size);
+		}
+	}
 }
